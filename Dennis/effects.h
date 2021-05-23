dad667b14fbf3e8f913370a9113813868f25d57a
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-05-23 12:47:16 +0200

    Create effects.h

---
+++ Dennis/effects.h
@@ -0,0 +1,15 @@
+class Effects {
+    public:
+        void test(){
+        }
+        void delay(){
+        }
+        void distortion(){
+
+        }
+        void reverb(){
+        }
+
+}
+
+Effects Fx;
