d4577667e1f4cf766de731f5c0fa5c5c3dec7055
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-05-23 12:41:49 +0200

    Update main.cpp

--- Dennis/main.cpp
+++ Dennis/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
 #include <display.h>
-#include <fx.h>
 
 void setup() {
-    Menu Display;                                       // Display int
     Display.start();                                    // Display Start
     Display.clear();                                    // Display Clear
     Display.rect();                                     // 
