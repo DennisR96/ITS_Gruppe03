f03ae67dc4e04d91768d6c42c7225d27c0728492
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-05-23 18:12:49 +0200

    Update main.cpp

--- Dennis/main.cpp
+++ Dennis/main.cpp
@@ -1,13 +1,29 @@
 #include <Arduino.h>
 #include <display.h>
+#include <audio_shield.h>
+
+
+// Int
+
+
 
 void setup() {
+    Serial.begin(115200);
+
     Display.start();                                    // Display Start
     Display.clear();                                    // Display Clear
-    Display.rect();                                     // 
+    Display.rect();                                     // Disp_rect
+
+    Teensy41.init();                                    // Init Audio Shield   
+    Teensy41.sound_test();                      
+
+    delay(1000);
+    
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("Teensy 4.1 - Audio FX");
 }
