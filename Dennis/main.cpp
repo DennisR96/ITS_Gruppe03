9401b588887e9e38f3e96329a3a9dca400dd382b
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-05-23 12:37:17 +0200

    Create main.cpp

---
+++ Dennis/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <display.h>
+#include <fx.h>
+
+void setup() {
+    Menu Display;                                       // Display int
+    Display.start();                                    // Display Start
+    Display.clear();                                    // Display Clear
+    Display.rect();                                     // 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
