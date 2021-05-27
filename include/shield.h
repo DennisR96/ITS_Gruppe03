e17186919f120db2cc066a09cd7cdc70e0867854
davyam1 <davyam@gmx.de>
2021-05-27 15:53:23 +0200

    2 Mixerstufen, 4 Effekte, 1 Filter, Bypass

--- include/shield.h
+++ include/shield.h
@@ -4,7 +4,10 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
+//USB
 AudioInputUSB            usb1;           //xy=316,164
+
+//Effekte
 AudioEffectChorus        chorus2;        //xy=545.7142791748047,76.28569412231445
 AudioEffectChorus        chorus1;        //xy=546.7143020629883,23.571426391601562
 AudioEffectWaveshaper    waveshape2;     //xy=565.0000267028809,173.28571701049805
@@ -14,13 +17,21 @@ AudioEffectWaveshaper    waveshape1;     //xy=575.4285049438477,132.142856597900
 AudioEffectDelay         delay1;         //xy=578.7143135070801,380.85711669921875
 AudioEffectDelay         delay2;         //xy=580.5714645385742,535.2857055664062
 AudioFilterStateVariable filter1;        //xy=583.3333396911621,683.3332786560059
+
+//Mixerstufe 1
 AudioMixer4              mixer3;         //xy=851.2697257995605,53.57143020629883
 AudioMixer4              mixer4;         //xy=854.1271324157715,140.7936553955078
 AudioMixer4              mixer6;         //xy=863.333309173584,333.015811920166
 AudioMixer4              mixer5;         //xy=866.5079574584961,265.7142906188965
+
+//Mixerstufe 2
 AudioMixer4              mixer2;         //xy=1006,191
 AudioMixer4              mixer1;         //xy=1007,126
+
+//Output
 AudioOutputI2S           i2s1;           //xy=1251,151
+
+//Patching
 AudioConnection          patchCord1(usb1, 0, chorus1, 0);
 AudioConnection          patchCord2(usb1, 0, reverb1, 0);
 AudioConnection          patchCord3(usb1, 0, delay1, 0);
@@ -47,5 +58,7 @@ AudioConnection          patchCord23(mixer6, 0, mixer2, 1);
 AudioConnection          patchCord24(mixer5, 0, mixer1, 1);
 AudioConnection          patchCord25(mixer2, 0, i2s1, 1);
 AudioConnection          patchCord26(mixer1, 0, i2s1, 0);
+
+//AudioControl
 AudioControlSGTL5000     sgtl5000_1;     //xy=162.7777557373047,246.11109924316406
 
