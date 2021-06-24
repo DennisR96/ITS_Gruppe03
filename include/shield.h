26d6da1d21b0df59c3d14577e2d569f4c533072a
dennisr96 <dennnisraek@gmail.com>
2021-06-24 14:21:11 +0200

    fre

--- include/shield.h
+++ include/shield.h
@@ -20,7 +20,7 @@ AudioConnection          patchCord4(i2s1, 1, mixer1, 3);
 //AudioConnection          patchCord5(freeverb1, biquad1);
 //AudioConnection          patchCord6(freeverb2, biquad2);
 AudioConnection          patchCord7(freeverb1, 0, mixer1, 1);
-AudioConnection          patchCord8(freeverb1, 0, mixer1, 0);
+AudioConnection          patchCord8(freeverb2, 0, mixer1, 0);
 AudioConnection          patchCord9(mixer1, fft1024);
 AudioConnection          patchCord10(mixer1, 0, i2s2, 0);
 AudioConnection          patchCord11(mixer1, 0, i2s2, 1);
