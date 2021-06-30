d690da1f7ede7a814ea469121e39c2851f259f05
davyam1 <davyam@gmx.de>
2021-06-30 16:27:55 +0200

    fft_new

--- include/encoding.h
+++ include/encoding.h
@@ -18,21 +18,21 @@ const long interval = 50;
 
 void fft_update(){ 
     if (i == 2 || i == -1){
-        level[0] =  fft1024.read(1);
-        level[1] =  fft1024.read(2);
-        level[2] =  fft1024.read(3);
-        level[3] =  fft1024.read(4);
-        level[4] =  fft1024.read(5, 7);
-        level[5] =  fft1024.read(7, 9);
-        level[6] =  fft1024.read(9, 13);
-        level[7] =  fft1024.read(13, 17);
-        level[8] =  fft1024.read(17, 23);
-        level[9] =  fft1024.read(23, 46);
-        level[10] = fft1024.read(46, 69);
-        level[11] = fft1024.read(69, 92);
-        level[12] = fft1024.read(92, 138);
-        level[13] = fft1024.read(138, 207);
-        level[14] = fft1024.read(207, 414);
+        level[0] =  fft1024.read(1,4);
+        level[1] =  fft1024.read(5,8);
+        level[2] =  fft1024.read(9,12);
+        level[3] =  fft1024.read(13,16);
+        level[4] =  fft1024.read(17,20);
+        level[5] =  fft1024.read(21.50);
+        level[6] =  fft1024.read(51, 80);
+        level[7] =  fft1024.read(81, 110);
+        level[8] =  fft1024.read(111, 140);
+        level[9] =  fft1024.read(141, 170);
+        level[10] = fft1024.read(171, 200);
+        level[11] = fft1024.read(201, 262);
+        level[12] = fft1024.read(263, 324);
+        level[13] = fft1024.read(325, 386);
+        level[14] = fft1024.read(387, 511);
         Display.fft_update(level);
     }       
         
